ae213d0d6924cd2ffcaf7325700072fb86f4ccb8
atomic-soap2022 <vladislav.v.k2007@gmail.com>
2023-01-04 11:12:44 +0200

    products

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -4,4 +4,5 @@ from  apps.catalog.views import CategoryIndexView
 urlpatterns = [
     path('',CategoryIndexView.as_view(),name = 'catalog'),
     path('<str:slug>/',CategoryIndexView.as_view(),name = 'categories'),
+
 ]
