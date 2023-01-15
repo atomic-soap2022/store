31e81d53494c76622bce6521806982f4357fc7c0
atomic-soap2022 <vladislav.v.k2007@gmail.com>
2023-01-15 07:22:25 +0200

    hw20

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,11 +1,21 @@
 from django.urls import reverse
 from django.views import generic
 from apps.catalog.models import Category, Product
-
 class CategoryIndexView(generic.ListView):
     model = Category
     template_name = 'catalog/index.html'
     queryset = Category.objects.filter(parent=None)
+
+    def set_breadcrumbs(self):
+        breadcrumbs = {'current': 'Каталог'}
+        return breadcrumbs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'breadcrumbs': self.set_breadcrumbs()})
+        return context
+
+
 class ProductsByCategoryView(generic.ListView):
     template_name = 'catalog/category.html'
     category = None
@@ -14,10 +24,8 @@ class ProductsByCategoryView(generic.ListView):
         self.category = Category.objects.get(slug=self.kwargs['slug'])
         queryset = Product.objects.filter(categories=self.category)
         return queryset
-
     def set_breadcrumbs(self):
         breadcrumbs = {reverse('catalog'): "Каталог"}
-
         category = self.category
         categories = []
         parent = category.parent
@@ -26,25 +34,19 @@ class ProductsByCategoryView(generic.ListView):
             parent = parent.parent
         for key, value in categories[::-1]:
             breadcrumbs.update({key: value})
-
         breadcrumbs.update({'current': self.category.name})
         return breadcrumbs
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = self.category
         context['categories'] = self.categories
         context['breadcrumbs'] = self.set_breadcrumbs()
         return context
-
-
 class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'catalog/product.html'
-
     def set_breadcrumbs(self):
         breadcrumbs = {reverse('catalog'): "Каталог"}
-
         category = self.object.main_category()
         if category:
             categories = [(reverse('categories', args=[category.slug]), category.name)]
@@ -54,10 +56,8 @@ class ProductDetailView(generic.DetailView):
                 parent = parent.parent
             for key, value in categories[::-1]:
                 breadcrumbs.update({key: value})
-
         breadcrumbs.update({'current': self.object.name})
         return breadcrumbs
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({'breadcrumbs': self.set_breadcrumbs()})
