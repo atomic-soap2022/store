6c1f1d23f9d63f83af54a82c1fff32b4e62bb446
atomic-soap2022 <vladislav.v.k2007@gmail.com>
2023-01-22 10:32:57 +0200

    dz24

--- apps/order/views.py
+++ apps/order/views.py
@@ -35,3 +35,6 @@ def add_to_cart(request):
             'order/added.html',
             {"product": cd['product'], "cart": get_cart_data(cd['user'])}
         )
+@login_required
+def cart_view(request):
+    return render(request, 'order/cart_view.html', {'cart': get_cart_data(request.user)})
