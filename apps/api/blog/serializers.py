e028397f83537bbe6ad36356ef9f2575e8f2a953
atomic-soap2022 <vladislav.v.k2007@gmail.com>
2023-02-18 20:21:14 +0200

    drf_dfjf

--- apps/api/blog/serializers.py
+++ apps/api/blog/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
-from apps.blog.models import Article
+from apps.blog.models import Article,Tag
+from apps.blog.models import BlogCategory
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id','name')
 
+class BlogCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogCategory
+        fields = ('id','name','image')
 
-class ArticleSerializer(serializers.ModelSerializer):
-
+class ArticleWriteSerializer(serializers.ModelSerializer):
+    tags = serializers.ListField(child=serializers.CharField(max_length=64))
     class Meta:
         model = Article
         fields = (
@@ -15,4 +24,24 @@ class ArticleSerializer(serializers.ModelSerializer):
             'title',
             'text_preview',
             'text',
+            'publish_date',
+            'tags'
+        )
+class ArticleReadSerializer(serializers.ModelSerializer):
+    category = BlogCategorySerializer()
+    tags = TagSerializer(many=True)
+    class Meta:
+        model = Article
+        fields = (
+            'id',
+            'category',
+            'user',
+            'image',
+            'title',
+            'text_preview',
+            'text',
+            'publish_date',
+            # 'image_thumbnail',
+            'created_at',
+            'tags'
         )
