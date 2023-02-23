a9757b3e653861525ac31a35ed3116925ef4ef1c
atomic-soap2022 <vladislav.v.k2007@gmail.com>
2023-02-23 14:36:32 +0200

    contacts_hw

--- apps/main/views.py
+++ apps/main/views.py
@@ -12,3 +12,13 @@ class PageView(generic.DetailView):
     model = Page
     template_name = 'main/page.html'
     queryset = Page.objects.all()
+
+    def set_breadcrumbs(self):
+        self.page = Page.objects.get(slug=self.kwargs['slug'])
+        breadcrumbs = {'current': self.page.name}
+        return breadcrumbs
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context.update({'breadcrumbs': self.set_breadcrumbs()})
+            return context
